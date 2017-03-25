26a4edae53a565c0e2aca062acaec03fe97b3c86
Mahendra Lakmal <mahendralakmal@Mahendras-MacBook-Pro.local>
2017-03-25 14:08:56 +0530

    update

--- admin/users_approve.php
+++ admin/users_approve.php
@@ -2,23 +2,20 @@
 
 require_once('../MysqliDb.php');
 $db = new MysqliDb ('localhost', 'homestead', 'secret', 'fsnhs');
+session_start();
 include('session.php');
 if (!empty($_GET['s'])) {
-
     $data = Array(
         'approved' => ($_GET['d']==1)? false:true,
         'updated_at' => $db->now(),
         'updated_by' => $_SESSION['login_id']
     );
 
-//    var_dump($data); exit();
-
     $id = $db->where('id', $_GET['s']);
     if ($id->update('users', $data))
         echo $id->count . ' records were updated';
     else
         echo 'update failed: ' . $id->getLastError();
-//    $id->update('users', $data);
 
     header("location: /admin/users_approvals.php");
 }
